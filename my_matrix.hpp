efff7faa73c6e9f94e45417dd8e31d492ad2fef7
AndreaCordone <andreacordo96@gmail.com>
2021-06-09 09:34:49 +0200

    added methods to perform scalar multiplication

--- my_matrix.hpp
+++ my_matrix.hpp
@@ -11,13 +11,22 @@ template <typename T >
 class Matrix {
 
 	public:
+		Matrix ( const uint & row, const uint & col) ; 
 		Matrix ( const uint & row, const uint & col, T matrix_element ) ;  
 		Matrix ( std::initializer_list<std::vector<T>> init_list ) ; 
-
-		T dimension (const uint & idx) ; 
+                Matrix ( const uint & row,const uint & col, std::vector<std::vector<T>>) ; 
+		
+		std::vector<std::vector<T>> get_matrix () const; 
+	
+		T dimension (const uint & idx) const ;
 		T & operator () ( const uint &row_idx, const uint &col_idx ) ; 
 		T operator () ( const uint &row_idx, const uint &col_idx ) const ; 
 		void Tr () ; 
+		
+		// Method for scalar multiplication
+		template <typename U>	
+		friend Matrix<U> operator * ( const Matrix<U> & a, const  U & scalar ) ; 
+
 	
 		template <typename U>
 		friend std::ostream& operator << ( std::ostream & out, const Matrix<U> & a) ;  		
@@ -30,7 +39,25 @@ class Matrix {
 
 } ;
 
- 
+
+
+// Constructor with a std::vector<std::vector<>>
+
+template <typename T> 
+Matrix<T>::Matrix (  const uint & n_row, const uint & n_col, std::vector<std::vector<T>> matrix_) 
+		: row(n_row)
+		, col(n_col)
+		, matrix(matrix_) {} 
+
+// Simple constructor  
+template <typename T>
+Matrix<T>::Matrix ( const uint & n_row, const uint & n_col) 
+		: row(n_row)
+		, col(n_col)
+		,matrix(row,std::vector<T>(col)) {} 
+
+
+
 // Constructor: Create a matrix with n_row and n_column with all elemt eqaul  
 template <typename T> 
 Matrix<T>::Matrix ( const uint & n_row, const uint & n_col, T matrix_element) 
@@ -67,7 +94,7 @@ T &  Matrix<T>::operator () (const uint &row_idx, const uint &col_idx)
 
 // Method to return the matrix dimension
 template <typename T> 
-T  Matrix<T>::dimension (const uint & idx)
+T  Matrix<T>::dimension (const uint & idx) const 
 {
 	if (idx == 1) { return this->row  ; }
 
@@ -105,10 +132,9 @@ for (int row_idx = 0; row_idx != this->row; ++row_idx) {
 		res[col_idx][row_idx] = this->matrix[row_idx][col_idx]  ;
 } }
 
-//std::copy ( res.begin(),res.end(),back_inserter(this->matrix)) ; 
-this->matrix.assign(res.begin(),res.end()) ; 
+ this->matrix.assign(res.begin(),res.end()) ; 
  
-const int temp = this->col ;
+ const int temp = this->col ;
  this->col = this->row ; 
  this->row = temp ; 
  
@@ -116,6 +142,24 @@ const int temp = this->col ;
 }
 
 
+template <typename T>
+std::vector<std::vector<T>> Matrix<T>::get_matrix () const { return this->matrix ; }  
+
+template <typename U>	
+Matrix<U> operator * (const Matrix<U> & a, const U & scalar ) {
 
 
+std::vector<std::vector<U>> src = a.get_matrix() ; 
+std::vector<std::vector<U>> dst(a.dimension(1),std::vector<U>(a.dimension(2),0))	;  
+
+for (auto it = std::make_pair(src.begin(),dst.begin()) ; it.first != src.end() ; ++it.first,++it.second ) {
+	
+std::transform(it.first->begin(), it.first->end(), it.second->begin(), [& scalar](auto & it_value ){ return scalar*it_value;});
+ 
+}
+
+Matrix<U> res (a.dimension(1),a.dimension(2),dst) ; 
+ 
+return  res ; 
+} 
 
